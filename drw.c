4bb15432ca960af388eaac52a2a493625401b8b2
qasimwarraich <qasim.warraich@gmail.com>
2020-07-17 01:13:09 +0200

    Fix for transparent borders from https://github.com/szatanjl/dwm/commit/1529909466206016f2101457bbf37c67195714c8

--- drw.c
+++ drw.c
@@ -205,6 +205,9 @@ drw_clr_create(Drw *drw, Clr *dest, const char *clrname)
 	                       DefaultColormap(drw->dpy, drw->screen),
 	                       clrname, dest))
 		die("error, cannot allocate color '%s'", clrname);
+    /* Fix for transparent terminal borders from: */
+    /*     https://github.com/szatanjl/dwm/commit/1529909466206016f2101457bbf37c67195714c8 */
+ 	dest->pixel |= 0xff << 24;
 }
 
 /* Wrapper to create color schemes. The caller has to call free(3) on the
