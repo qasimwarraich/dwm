5862a5fe83dbb8e98b5b128251f99b29cc6774d4
qasimwarraich <qasim.warraich@gmail.com>
2020-07-24 02:02:08 +0200

    Removed useless gaps patch

--- config.def.h
+++ config.def.h
@@ -4,7 +4,6 @@
 static const unsigned int baralpha        = 0xd0;    /* alpha bar patch */
 static const unsigned int borderalpha     = OPAQUE;  /* alpha bar patch */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
