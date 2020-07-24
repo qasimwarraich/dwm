1b26ee949686fe56993cd684491026dcf60c7f44
qasimwarraich <qasim.warraich@gmail.com>
2020-07-24 02:46:47 +0200

    Added tile gap patch appears to be playing well with cfacts

--- config.def.h
+++ config.def.h
@@ -4,6 +4,7 @@
 static const unsigned int baralpha        = 0xd0;    /* alpha bar patch */
 static const unsigned int borderalpha     = OPAQUE;  /* alpha bar patch */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 18;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
